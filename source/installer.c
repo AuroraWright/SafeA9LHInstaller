95020a5be89aca513db6cb8bcb8e0b171828acf5
TheBaloneyboy <michaelman00@me.com>
2016-05-21 16:51:11 -0700

    Eh, I'll ask #cakey

--- source/installer.c
+++ source/installer.c
@@ -11,9 +11,14 @@ static const u8 sectorHash[0x20] = {
     0xBA, 0xB2, 0x4B, 0x4E, 0x5F, 0x65, 0xC9, 0x81, 0xCD, 0x7B, 0xE6, 0xF4, 0x38, 0xE6, 0xD9, 0xD3
 };
 
-static const u8 sectorA9lhHash[0x20] = {
+static const u8 sectorA9lhHashNew[0x20] = {
     0xBB, 0xC2, 0x45, 0xE7, 0xCE, 0x6A, 0x66, 0x51, 0x7C, 0x43, 0x95, 0xA6, 0xCB, 0x82, 0x7A, 0x46,
     0x90, 0x0E, 0xE8, 0x1A, 0xF1, 0xD2, 0x09, 0x9F, 0x7B, 0x92, 0xD3, 0x98, 0xED, 0x17, 0x72, 0xCF
+};
+
+static const u8 sectorA9lhHashOld[0x20] = {
+    0x89, 0x72, 0xAD, 0x96, 0x42, 0x6F, 0x8A, 0x9B, 0x3E, 0xEB, 0x4C, 0xC9, 0xCC, 0xEF, 0x0E, 0xF4,
+    0x5B, 0x91, 0x91, 0xFB, 0xEE, 0xFC, 0x7E, 0x30, 0xB4, 0x8E, 0xE3, 0x1A, 0x3E, 0xD0, 0x42, 0x3A
 };
 
 static const u8 firm0Hash[0x20] = {
@@ -21,9 +26,14 @@ static const u8 firm0Hash[0x20] = {
     0x4F, 0x72, 0x4B, 0xBD, 0x97, 0x39, 0x33, 0xF2, 0x11, 0xC9, 0x35, 0x22, 0xC8, 0xBB, 0x1C, 0x7D
 };
 
-static const u8 firm0A9lhHash[0x20] = {
+static const u8 firm0A9lhHashNew[0x20] = {
     0xDB, 0x04, 0x3D, 0x35, 0x62, 0x65, 0xDF, 0x25, 0xD0, 0xC8, 0x7B, 0xFE, 0xC4, 0xF5, 0x10, 0x3B,
     0x33, 0x81, 0xF7, 0x07, 0x2A, 0xD4, 0xF8, 0xD9, 0x78, 0xD5, 0x57, 0xB1, 0xF9, 0x06, 0xD2, 0xDE
+};
+
+static const u8 firm0A9lhHashOld[0x20] = {
+    0xFC, 0x7C, 0xC7, 0x2D, 0xA3, 0x0A, 0xDE, 0x3A, 0xE2, 0xFE, 0x7D, 0x79, 0xA3, 0xC2, 0x6C, 0x82,
+    0xB8, 0xA3, 0xDB, 0xC4, 0x46, 0x22, 0xB4, 0x61, 0x8D, 0xDC, 0x73, 0x90, 0x50, 0x5D, 0x40, 0x11
 };
 
 static const u8 firm1Hash[0x20] = {
@@ -33,6 +43,16 @@ static const u8 firm1Hash[0x20] = {
 
 int pos_y;
 
+void checkSector(void) {
+    char* path;
+    path = "a9lh/secret_sector.bin";
+    if(fileSize(path) != 0x200)
+        shutdown(1, "Error: secret_sector.bin doesn't exist or has\na wrong size");
+    fileRead((void *)SECTOR_OFFSET, path, 0x200);
+    if(!verifyHash((void *)SECTOR_OFFSET, 0x200, sectorHash))
+        shutdown(1, "Error: secret_sector.bin is invalid or corrupted");
+}
+
 void installer(void){
     //Determine if booting with A9LH
     u32 a9lhBoot = (PDN_SPI_CNT == 0x0) ? 1 : 0;
@@ -41,7 +61,7 @@ void installer(void){
 
     drawString("Safe A9LH Installer v2.0", 10, 10, COLOR_TITLE);
     pos_y = drawString("Thanks to delebile, #cakey and StandardBus", 10, 40, COLOR_WHITE);
-    pos_y = drawString("Modified by TheBaloneyboy", 10, POS_Y, COLOR_WHITE);
+    pos_y = drawString("Modified by TheBaloneyboy", 10, pos_y, COLOR_WHITE);
     pos_y = drawString(a9lhBoot ? "Press SELECT to update A9LH" : "Press SELECT for a full install", 10, pos_y + SPACING_VERT, COLOR_WHITE);
     pos_y = drawString("Press START to install an alternate stage2 (EXPERIMENTAL)", 10, pos_y, COLOR_WHITE);
     pos_y = drawString("Press any other button to shutdown", 10, pos_y, COLOR_WHITE);
@@ -71,48 +91,42 @@ void installer(void){
     if(memcmp((void *)FIRM0_OFFSET, "FIRM", 4) != 0)
         shutdown(1, "Error: failed to setup FIRM encryption");
 
-    //If booting from A9LH or on N3DS, we can use the key sector from NAND
-    if(a9lhBoot || console){
-         getSector((u8 *)SECTOR_OFFSET);
-         if(!verifyHash((void *)SECTOR_OFFSET, 0x200, a9lhBoot ? sectorA9lhHash : sectorHash))
-             shutdown(1, a9lhBoot ? "Error: the OTP hash or the NAND key sector\nare invalid" :
-                                    "Error: the otp.bin is invalid or corrupted,\nor you have already installed A9LH");
-    } else {
-         //Read decrypted key sector
-         path = "a9lh/secret_sector.bin";
-         if(fileSize(path) != 0x200)
-             shutdown(1, "Error: secret_sector.bin doesn't exist or has\na wrong size");
-         fileRead((void *)SECTOR_OFFSET, path, 0x200);
-         if(!verifyHash((void *)SECTOR_OFFSET, 0x200, sectorHash))
-             shutdown(1, "Error: secret_sector.bin is invalid or corrupted");
-    }
+    //TODO: Reimplement on the fly sector grabbing.
+    path = "a9lh/secret_sector.bin";
+    if(fileSize(path) != 0x200)
+    shutdown(1, "Error: secret_sector.bin doesn't exist or has\na wrong size");
+    fileRead((void *)SECTOR_OFFSET, path, 0x200);
+    if(!verifyHash((void *)SECTOR_OFFSET, 0x200, sectorHash))
+    shutdown(1, "Error: secret_sector.bin is invalid or corrupted");
+    
+    
+    //Generate and encrypt a per-console A9LH key sector
+    generateSector((u8 *)SECTOR_OFFSET);
 
-    if(!a9lhBoot){
-        //Generate and encrypt a per-console A9LH key sector
-        generateSector((u8 *)SECTOR_OFFSET);
+    //TODO: Reimplement as-needed FIRM checks
 
-        //Read FIRM0
-        path = "a9lh/firm0.bin";
-        if(fileSize(path) != FIRM_SIZE)
-            shutdown(1, "Error: firm0.bin doesn't exist or has a wrong size");
-        fileRead((void *)FIRM0_OFFSET, path, FIRM_SIZE);
-        if(!verifyHash((void *)FIRM0_OFFSET, FIRM_SIZE, firm0Hash))
-            shutdown(1, "Error: firm0.bin is invalid or corrupted");
+    //Read FIRM0
+    path = "a9lh/firm0.bin";
+    u32 size = fileSize(path);
+    if(size != FIRM_SIZE)
+        shutdown(1, (char) size);
+    fileRead((void *)FIRM0_OFFSET, path, FIRM_SIZE);
+    if(!verifyHash((void *)FIRM0_OFFSET, FIRM_SIZE, firm0Hash))
+        shutdown(1, "Error: firm0.bin is invalid or corrupted");
 
-        //Read FIRM1
-        path = "a9lh/firm1.bin";
-        if(fileSize(path) != FIRM_SIZE)
-            shutdown(1, "Error: firm1.bin doesn't exist or has a wrong size");
-        fileRead((void *)FIRM1_OFFSET, path, FIRM_SIZE);
-        if(!verifyHash((void *)FIRM1_OFFSET, FIRM_SIZE, firm1Hash))
-            shutdown(1, "Error: firm1.bin is invalid or corrupted");
-    }
-    else if(!verifyHash((void *)FIRM0_OFFSET, STAGE1_POSITION, firm0A9lhHash))
-        shutdown(1, "Error: NAND FIRM0 is invalid");
+    //Read FIRM1
+    path = "a9lh/firm1.bin";
+    if(fileSize(path) != FIRM_SIZE)
+        shutdown(1, "Error: firm1.bin doesn't exist or has a wrong size");
+    fileRead((void *)FIRM1_OFFSET, path, FIRM_SIZE);
+    if(!verifyHash((void *)FIRM1_OFFSET, FIRM_SIZE, firm1Hash))
+        shutdown(1, "Error: firm1.bin is invalid or corrupted");
+
+    
 
     //Inject stage1
     path = "a9lh/payload_stage1.bin";
-    u32 size = fileSize(path);
+    size = fileSize(path);
     if(!size || size > MAX_STAGE1_SIZE)
         shutdown(1, "Error: stage1.bin doesn't exist or exceeds\nmax size");
     memset((void *)STAGE1_OFFSET, 0, MAX_STAGE1_SIZE);
